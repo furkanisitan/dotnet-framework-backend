9eaae04631168c02a741aac4d6de96fe0eb4d1b5
furkanisitan <furkanisitan96@gmail.com>
2021-01-14 00:40:02 +0300

    dependency injection structure created

---
+++ Shop.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,26 @@
+﻿using Ninject.Modules;
+using Shop.Business.Abstract;
+using Shop.Business.Concrete.Managers;
+using Shop.DataAccess.Abstract;
+using Shop.DataAccess.Concrete.EntityFramework;
+
+namespace Shop.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+
+            Bind<ICategoryService>().To<CategoryManager>().InSingletonScope();
+            Bind<ICategoryDal>().To<EfCategoryDal>().InSingletonScope();
+
+            Bind<IUserService>().To<UserManager>().InSingletonScope();
+            Bind<IUserDal>().To<EfUserDal>().InSingletonScope();
+
+            Bind<IRoleService>().To<RoleManager>().InSingletonScope();
+            Bind<IRoleDal>().To<EfRoleDal>().InSingletonScope();
+        }
+    }
+}
