5be3970767d9ad9f5e07f1519133af2f04f6d59f
furkanisitan <furkanisitan96@gmail.com>
2021-01-15 18:32:38 +0300

    FluentValidationAspect created and tested

---
+++ Shop.Business.Tests/ValidationTests/FluentValidation/FluentValidationAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Business.ValidationRules.FluentValidation;
+using Shop.Entities.Concrete;
+using System.Reflection;
+
+namespace Shop.Business.Tests.ValidationTests.FluentValidation
+{
+    [TestClass]
+    public class FluentValidationAttributeTests
+    {
+
+        [TestMethod]
+        public void Test_instance_method()
+        {
+            var ex = Assert.ThrowsException<TargetInvocationException>(() => InstanceMethod(new Product { Name = "" }));
+            Assert.IsTrue(ex.InnerException is ValidationException);
+        }
+
+        [FluentValidation(typeof(ProductValidator))]
+        private void InstanceMethod(Product product) { }
+
+
+    }
+}
---
+++ Shop.Business/ValidationRules/FluentValidation/FluentValidationAttribute.cs
@@ -0,0 +1,38 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using Shop.Business.DependencyResolvers.Ninject;
+using Shop.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using System;
+using System.Linq;
+
+namespace Shop.Business.ValidationRules.FluentValidation
+{
+    /// <summary>
+    /// Validates parameters of type 'IEntity'
+    /// </summary>
+    [PSerializable]
+    public sealed class FluentValidationAttribute : OnMethodBoundaryAspect
+    {
+        private Type _validatorType;
+        private string[] _ruleSets;
+
+        /// <param name="validatorType">Type of validation class</param>
+        /// <param name="ruleSets">The names of the ruleSets to validate. (optional)</param>
+        public FluentValidationAttribute(Type validatorType, params string[] ruleSets)
+        {
+            _validatorType = validatorType;
+            _ruleSets = ruleSets;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var entityType = _validatorType.BaseType?.GetGenericArguments()[0];
+            var entities = args.Arguments.Where(x => x.GetType() == entityType);
+            var genericValidateMethod = typeof(ValidatorTool).GetMethod(nameof(ValidatorTool.FluentValidate))?.MakeGenericMethod(entityType);
+            var validator = InstanceFactory.GetInstance(_validatorType);
+
+            foreach (var entity in entities)
+                genericValidateMethod?.Invoke(null, new[] { validator, entity, _ruleSets });
+        }
+    }
+}
