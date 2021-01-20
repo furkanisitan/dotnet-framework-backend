98ef6c47ef080d781d49ffd373a36e37b02f21d1
furkanisitan <furkanisitan96@gmail.com>
2021-01-20 23:30:45 +0300

    PerformanceCounterAspect created and tested

---
+++ Shop.Business.Tests/AspectTests/Postsharp/PerformanceCounterAttributeTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Business.Aspects.Postsharp.PerformanceAspects;
+using System.Threading;
+
+namespace Shop.Business.Tests.AspectTests.Postsharp
+{
+    [TestClass]
+    public class PerformanceCounterAttributeTests
+    {
+
+        [TestMethod]
+        [PerformanceCounter]
+        public void Should_WarningOnDebugTrace_When_RunningTimeMoreThan5Seconds()
+        {
+            Thread.Sleep(5000);
+            // check DebugTrace
+        }
+    }
+}
